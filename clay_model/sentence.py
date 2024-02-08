f3596bf5df1128695ad20b15b97023340549697e
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-02-08 19:58:46 +0900

    init commit

---
+++ clay_model/sentence.py
@@ -0,0 +1,14 @@
+from enum import Enum,auto
+
+# debug tools
+from pprint import pprint
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+class parser:
+
+    def __init__(self,code):
+        self.code=code
+        pass
