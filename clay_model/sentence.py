4ffb1e76cce134000ba36dc9a051560a895b519f
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-02-24 10:14:10 +0900

    edit sentence.py file

--- clay_model/sentence.py
+++ clay_model/sentence.py
@@ -3,6 +3,7 @@ from enum import Enum,auto
 # debug tools
 from pprint import pprint
 import logging
+import copy
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -27,11 +28,29 @@ class Object_tag(Enum):
     # 単語
     WORD = auto()
 
+class Expr_parser:
+    # resolve <expr>
+    # 式を解決します
+    def __init__(self,code:str,mode="expr") -> None:
+        self.code = code
+    
+    def list_resolve():
+        pass
+
+
+class Expr_element:
+    def __init__(self) -> None:
+        pass
+    
+    
+
 
 class Proc_parser:
+    # resolve <proc>
+    # 処理を解決します
     def __init__(self, code:str) -> None:
         self.code = code
-        
+
         # const
         self.OPENBRACE = '{'
         self.CLOSEBRACE = '}'
@@ -46,19 +65,20 @@ class Proc_parser:
         self.SINGLEQUOTATION = '\''
         self.ESCAPESTRING = "\\"
 
-    def resolve0(self,code:str) -> list["parse_element"]:
+    def resolve0(self,code:str) -> list["proc_element"]:
         codelist = self.resolve_quatation(code,self.DOUBLEQUOTATION)
         return codelist
 
-    def resolve1(self,codelist:list["parse_element"]) -> list["parse_element"]:
+    def resolve1(self,codelist:list["proc_element"]) -> list["proc_element"]:
         codelist = self.resolve_block(codelist) # {}
+        codelist = self.resolve_block2(codelist)
         return codelist
 
-    def resolve2(self,codelist:list["parse_element"]):
+    def resolve2(self,codelist:list["proc_element"]):
         return codelist
 
-    # クォーテーションをまとめる
-    def resolve_quatation(self,strlist:list[str], quo_char:str) -> list["parse_element"]:
+    def resolve_quatation(self,strlist:list[str], quo_char:str) -> list["proc_element"]:
+        # クォーテーションをまとめる
         open_flag:bool = False
         escape_flag:bool = False
         rlist:list = list()
@@ -80,7 +100,7 @@ class Proc_parser:
                     if open_flag:
                         #group.append(i)
                         rlist.append(
-                            parse_element.new(Object_tag.STRING,None,None,"".join(group),position,len(group))
+                            proc_element.new(Object_tag.STRING,None,None,"".join(group),position,len(group))
                         )
                         group.clear()
                         open_flag = False
@@ -96,18 +116,20 @@ class Proc_parser:
                             escape_flag = False
                         group.append(inner)
                     else:
-                        rlist.append(parse_element.new(Object_tag.UNDEF,None,None,inner,(newline_counter,column_counter),1)) # undefのときはそのまま
+                        rlist.append(
+                            proc_element.new(Object_tag.UNDEF,None,None,inner,(newline_counter,column_counter),1)
+                        ) # undefのときはそのまま
         return rlist
 
     # ブロックごとにまとめる
-    def resolve_block(self,strlist:list["parse_element"]) -> list["parse_element"]:
+    def resolve_block(self,strlist:list["proc_element"]) -> list["proc_element"]:
         depth:int = 0
         rlist:list = list()
         group:list = list()
         for par_elem in strlist:
             inner = par_elem.contents
             elem_type_tag = par_elem.get_type
-            
+
             if elem_type_tag is Object_tag.UNDEF and inner == "{":
                 if depth>0:
                     group.append(par_elem)
@@ -127,7 +149,9 @@ class Proc_parser:
                     #group.append(i)
                     contents = self.resolve1(group)
                     contents_length = sum(map(lambda a:a.length,contents))
-                    rlist.append(parse_element(Object_tag.BLOCK,None,None,contents,position,contents_length + 2))
+                    rlist.append(
+                        proc_element(Object_tag.BLOCK,None,None,contents,position,contents_length + 2)
+                    )
                     group.clear()
                 else:
                     print("Error!")
@@ -142,17 +166,55 @@ class Proc_parser:
                     return 
         return rlist
 
-    def resolve_block2(self,strlist:list["parse_element"]):
+    def resolve_block2(self,strlist:list["proc_element"]):
+
+        depth:int = 0
         group = list()
         rlist = list()
-
+        position:tuple[int,int]
         for par_elem in strlist:
-            if par_elem.get_type is Object_tag.UNDEF :
-                pass
-
+            inner = par_elem.contents
+            elem_type_tag = par_elem.get_type
+            if elem_type_tag is Object_tag.UNDEF and inner == "(":
+                if depth > 0:
+                    group.append(par_elem)
+                elif depth == 0:
+                    position = par_elem.position
+                else:
+                    print("Error!")
+                    return
+                depth += 1
+            elif elem_type_tag is Object_tag.UNDEF and inner == ")":
+                depth -= 1
+                if depth > 0:
+                    group.append(par_elem)
+                elif depth == 0:
+                    contents = group # 工事中
+                    contents_length = sum(map(lambda a:a.length,contents))
+                    if group:
+                        rlist.append(proc_element.new(
+                            type_ = Object_tag.PARENBLOCK,
+                            word = None,
+                            expr = copy.copy(contents),
+                            contents = None,
+                            position = position,
+                            length = contents_length
+                        ))
+                        group.clear()
+                else:
+                    print("Error!")
+                    return
+            else:
+                if depth>0:
+                    group.append(par_elem)
+                elif depth == 0:
+                    rlist.append(par_elem)
+                else:
+                    print("Error!")
+                    return
         return rlist
 
-    def grouping_word(self,strlist:list["parse_element"], split_str:str, control_str:str) -> list["parse_element"]:
+    def grouping_word(self,strlist:list["proc_element"], split_str:str, control_str:str) -> list["proc_element"]:
         """
         文法規則に基づいて、単語をまとめる
         split_str = ["\t","\n"," "]
@@ -170,7 +232,7 @@ class Proc_parser:
                 if par_elem.contents in split_str: # 区切り文字であった場合
                     if group:                     # もし単語グループがあれば
                         word = "".join(group)
-                        rlist.append(parse_element.new(Object_tag.WORD,word, None, None))
+                        rlist.append(proc_element.new(Object_tag.WORD,word, None, None))
                         group.clear()
                     else:                         # なければ
                         rlist.append(par_elem)
@@ -179,14 +241,14 @@ class Proc_parser:
             elif par_elem.get_type is Object_tag.BLOCK: # ブロックである場合
                 if group:                               # 単語グループがあれば
                     word = "".join(group)
-                    rlist.append( parse_element.new(Object_tag.WORD, word, None, None))
+                    rlist.append( proc_element.new(Object_tag.WORD, word, None, None))
                     group.clear()
                 rlist.append(par_elem)
             else:                                       # グループでも未定義でもない場合
                 rlist.append(par_elem)
         return rlist
 
-    def grouping_control_syntax0(self, strlist:list["parse_element"], syntax_strings:list[str]) -> list["parse_element"]:
+    def grouping_control_syntax0(self, strlist:list["proc_element"], syntax_strings:list[str]) -> list["proc_element"]:
         # expr<式>の部分を取り出す
         # # これは次の処理で
         # a[<expr>] #要素が1
@@ -216,13 +278,13 @@ class Proc_parser:
             elif par_elem.get_type is Object_tag.UNDEF and par_elem.contents == ";" and expr_flag0:
                 # close
                 if group:
-                    rlist.append(parse_element(Object_tag.EXPR,None,"".join(group),None))
+                    rlist.append(proc_element(Object_tag.EXPR,None,"".join(group),None))
                 expr_flag0=False
             elif par_elem.get_type is Object_tag.BLOCK and expr_flag1:
                 # close
                 depth -= 1
                 if group and depth == 0:
-                    rlist.append(parse_element(Object_tag.EXPR,None,"".join(group),None))
+                    rlist.append(proc_element(Object_tag.EXPR,None,"".join(group),None))
                     expr_flag1 = False
             else:
                 if expr_flag0 or expr_flag1:
@@ -231,14 +293,14 @@ class Proc_parser:
                     rlist.append(par_elem)
         return rlist
 
-    def resolve_control_syntax(self, strlist:list["parse_element"], syntax_string:list[str]) -> list["parse_element"]:
+    def resolve_control_syntax(self, strlist:list["proc_element"], syntax_string:list[str]) -> list["proc_element"]:
         # resolve_iewfの新しいバージョン
         grouped_strlist = self.grouping_control_syntax0(strlist,syntax_string) # とりあえずまとめる
 
 
-class parse_element:
+class proc_element:
     # tree状になったparseオブジェクト
-    def __init__(self,type_:Object_tag,word:str,expr:"parse_element",contents:"parse_element",position:tuple[int,int],length:int):
+    def __init__(self,type_:Object_tag,word:str,expr:"proc_element",contents:"proc_element",position:tuple[int,int],length:int):
         # position (ln,col)
         self.type_ = type_
         self.word = word
@@ -248,8 +310,8 @@ class parse_element:
         self.length = length
 
     @classmethod
-    def new(cls,type_:Object_tag,word:str,expr:str,contents:"parse_element",position:tuple[int,int],length:int) -> "parse_element":
-        return parse_element(type_,word,expr,contents,position,length)
+    def new(cls,type_:Object_tag,word:str,expr:str,contents:"proc_element",position:tuple[int,int],length:int) -> "proc_element":
+        return proc_element(type_,word,expr,contents,position,length)
 
     @property
     def get_type(self):
@@ -258,107 +320,18 @@ class parse_element:
     def __repr__(self) -> str:
         return f"type ({self.type_}) expr ({self.expr}) word ({self.word}) contents ({self.contents}) (Ln: {self.position[0]},col: {self.position[1]})\n"
 
-#test
+# test
 
 def __test_00():
-    code_sample_00:str = """
-    fn fizzbazz(a:i32):void{
-        if a % 15 == 0{
-            print("fizzbazz");
-        }elif a % == 3{
-            print("fizz");
-        }elif a % == 5{
-            print("bazz");
-        }else{
-            print(a);
-        }
-    }
-    fn main(){
-        // コメント、コメント
-        for i in 0..100 {
-            fizzbazz(i);
-        }
-    }
-    """
-    code_sample_01 = """
-    fn gcd(a:i32,b:i32):i32{
-        if b == 0{
-            return a;
-        }else{
-            return gcd(b,a%b);//コメント
-        }
-    }
-    fn main(){
-        let a = 123456789;
-        let b = 987654321;
-        debug("gcd(%d, %d) -> %d", a, b, gcd(a, b));
-    }
-    """
-
-def __test_01():
-    """
-    # single or double quotation test
-    """
-    a = Proc_parser("hello")
-    code = """
-let code : String = "\\"hello\\"";
-let code : String = "hello 
-
-world";
-"""
-    print (
-        a.resolve_quatation(code,a.DOUBLEQUOTATION)
-    )
-
-def __test_02():
-    """
-    # brace test
-    """
     a = Proc_parser("")
-    code = """
-a[0:5]
-{hello world}
-if a == "{"{
-    print("hello world")
-}
-
-fn main (void){
-    if (){
-        print("hello world"[0:5])
-    }
-}
-
-"""
-    codelist = a.resolve_quatation(code,a.DOUBLEQUOTATION)
-    codelist = a.resolve_block(codelist,a.OPENSQUAREBRACKET,a.CLOSESQUAREBRACKET,Object_tag.SQBLOCK)
-    codelist = a.resolve_block(codelist,a.OPENBRACE,a.CLOSEBRACE,Object_tag.BLOCK)
-    for i,j in enumerate(codelist):
-        print(str(i).rjust(2),j)
-
-def __test_03():
-    a = Proc_parser("")
-    code = """
-const list = ["hello","world"];
-const string = "world";
-
-fn add(a:i32,b:i32){
-    return a+b;
-}
-
-fn main ():int{
-    for i in list{
-        const flag = string==i;
-        if if flag {1} else {0}{
-            const a = "hello" + "world";
-        print("hello" + "world");
-}}}
-"""
+    with open("../examples/ex02.lc",mode="r",encoding="utf-8") as f:
+        code = f.read()
     codelist = a.resolve0(code)
     codelist = a.resolve1(codelist)
-    print(list(map(lambda b:"<BLOCK>" if b.get_type is Object_tag.BLOCK else b,codelist)))
-    #print(codelist)
+    #print(list(map(lambda b:"<BLOCK>" if b.get_type is Object_tag.BLOCK else b,codelist)))
+    print(codelist)
 
-def __test_04():
+def __test_01():
     a = Proc_parser("")
     code = """
 const list = [[1,1,1],[0,0,0],[1,1,1]];
@@ -377,4 +350,4 @@ for i in list{
 
 
 if __name__=="__main__":
-    __test_03()
+    __test_00()
