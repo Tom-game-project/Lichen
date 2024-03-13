29742bffa83f78f5d6a382babe67872c5db23966
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-03-13 17:20:03 +0900

    add enum ParseElem and impl resolve_quotation method

--- lichen-lang/src/lib.rs
+++ lichen-lang/src/lib.rs
@@ -5,15 +5,16 @@
 struct Parser{
     code:String,
     //priority list(演算子優先度リスト)
-    left_priority_list:Vec<(String,i32)>,
-    right_priority_list:Vec<(String,i32)>,
-    prefix_priority_list:Vec<(String,i32)>,
+    left_priority_list: Vec<(String,i32)>,
+    right_priority_list: Vec<(String,i32)>,
+    prefix_priority_list: Vec<(String,i32)>,
     plusminus_prefix_priority:i32,
 }
 
 
 impl Parser{
-    fn new(&self,code:&str) -> Self{
+
+    fn new(code:&str) -> Self{
         Self {
             code: code.to_string(),
             left_priority_list: vec![
@@ -46,35 +47,151 @@ impl Parser{
         }
     }
 
-    fn resolve_quotation <T:Elem>(&self) -> Vec<T>{
-        todo!()
+    fn resolve_quotation(&self,code:String,quo_char:char) -> Vec<ParseElem>{
+        let mut open_flag = false;
+        let mut escape_flag = false;
+        let mut rlist:Vec<ParseElem> = Vec::new();
+        let mut group:Vec<char> = Vec::new();
+        let mut newline_counter:usize = 0;
+        let mut column_counter:usize = 0;
+
+        for i in code.chars().into_iter(){
+            if i == '\n'{
+                column_counter = 0;
+                newline_counter += 1;
+            }else{
+                column_counter += 1
+            }
+
+            if escape_flag{
+                group.push(i);
+                escape_flag = false;
+            }else{
+                if i == quo_char{
+                    if open_flag{
+                        group.push(i);
+                        println!("{:?}",group);
+                        let se = StringElem::new(
+                            group
+                            .clone()
+                            .into_iter()
+                            .collect()
+                        );
+                        rlist.push(
+                            ParseElem::StringParseElem(se)
+                        );
+                        group.clear();
+                        open_flag = false;
+                    }else{
+                        group.push(i);
+                        open_flag = true;
+                    }
+                }else{
+                    if open_flag{
+                        if i == '\\'{
+                            escape_flag = true;
+                        }else{
+                            escape_flag = false;
+                        }
+                        group.push(i);
+                    }else{
+                        let ue = UndefElem::new(i.clone().to_string());
+                        rlist.push(ParseElem::UndefParseElem(ue));
+                    }
+                }
+            }
+        }
+        return rlist;
     }
 }
 
 
-trait Elem {
-    fn new(&self) -> Self where Self: Sized{
-        todo!();
+/// # 列挙型
+/// 
+
+
+/// # 前要素共通メソッド
+///
+trait Elem{
+
+}
+
+
+/// # StringElem
+/// ## 文字列
+struct StringElem{
+    contents:String
+}
+impl StringElem{
+    fn new(contents:String) -> Self{
+        Self{
+            contents:contents
+        }
     }
-    fn get_name(&self) -> String;
-    fn get_contents(&self) -> Vec<String>;
 }
 
-struct String_Elem{
-
+/// # WordElem
+/// ## 単語を格納
+struct WordElem{
+    contents:String
+}
+impl WordElem{
+    fn new(contents:String) -> Self{
+        Self{
+            contents:contents
+        }
+    }
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+/// # UndefElem
+/// ## 不定な型
+struct UndefElem{
+    contents:String
 }
+impl UndefElem{
+    fn new(contents:String) -> Self{
+        Self{
+            contents:contents
+        }
+    }
+}
+
+/// # プログラムの要素
+enum ParseElem{
+    StringParseElem(StringElem),
+    WordParseElem(WordElem),
+    UndefParseElem(UndefElem),
+}
+impl ParseElem{
+    fn show(&self){
+        match self{
+            ParseElem::StringParseElem(v) => {
+                println!("<string: {}>",v.contents);
+            }
+            ParseElem::WordParseElem(v) => {
+                println!("<word: {}>",v.contents);
+            }
+            ParseElem::UndefParseElem(v) => {
+                println!("{}",v.contents);
+            }
+        }
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_00() {
+        let code = "a = \"hello world\";".to_string();
+        let mut parser = Parser::new(&code);
+        let codelist=parser.resolve_quotation(code, '"');
+        for i in codelist{
+            i.show();
+        }
     }
 }
